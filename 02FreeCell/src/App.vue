30386c59b5c14297f49e2e74b5e8c1e80af4829b
ItsMeKevin1227 <c.k.louis1227@gmail.com>
2019-07-22 21:08:59 +0800

    first commit

---
+++ 02FreeCell/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  width: 100%;
+  height: 100vh;
+  background-image: url('./assets/bg-all.png');
+  background-size: contain;
+  background-position: 50% 50%;
+}
+</style>
---
+++ 02FreeCell/vue.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, `./${dir}`)
+}
+
+module.exports = {
+  publicPath: './',
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    // 自動導入less
+    types.forEach(type =>
+      addStyleResource(config.module.rule('less').oneOf(type))
+    )
+
+    // import alias
+    config.resolve.alias
+      .set('Stores', resolve('@/stores'))
+      .set('Views', resolve('@/views'))
+      .set('Components', resolve('@/components'))
+      .set('Assets', resolve('@/assets'))
+      .set('Styles', resolve('@/styles'))
+  },
+  // 自動導入less
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    },
+    extract: false
+  }
+}
+
+// 自動導入less
+function addStyleResource (rule) {
+  rule
+    .use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [path.resolve(__dirname, './src/styles/general.less')]
+    })
+}
